0809c45053dd4795651b671b29e2e89766645833
Pavel <pavel@b2profit.ru>
2024-10-05 21:19:50 +0300

    1 семинар

--- Sem_1/script.js
+++ Sem_1/script.js
@@ -1,20 +1,24 @@
 console.log('Текст из script.js');
 let a = 1.5;
 let b = 0.75;
-console.log(a+b);
+console.log(a + b);
 a = 45;
 console.log(-a);
 a = 'Hello';
 b = 'World';
-console.log(a+' '+b);
+console.log(a + ' ' + b);
 
-let val1 = 6;
-let val2 = 3;
+let val1 = 10;
+let val2 = 2;
 let summ = val1 + val2;
 console.log('Сумма = ', summ);
 let diff = val1 - val2;
 console.log('Разница = ', diff);
-let product = val1 * val2;
-console.log('Произведение = ', product);
-let quotient = val1 / val2;
-console.log('Частное = ', quotient);
+let multiply = val1 * val2;
+console.log('Произведение = ', multiply);
+let divide = val1 / val2;
+console.log('Частное = ', divide);
+console.log('Сумма + Разница + Произведение + Частное = ', divide + multiply + diff + summ);
+
+
+
