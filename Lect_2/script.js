c52eac656a99a97cd34b1c6f87493a7a125e2325
Pavel <pavel@b2profit.ru>
2024-10-13 17:36:11 +0300

    2 лекция

--- Lect_2/script.js
+++ Lect_2/script.js
@@ -1,1 +1,12 @@
 console.log('Привет!!');
+console.log(2==2); // true
+console.log('2'==2); // true - js сам приводит типы переменных == - нестрогое равенство
+
+console.log(2===2); // true 
+console.log('2'===2); // false === - строгое равенство с учётом типа данных
+
+console.log(2!==5); // true 
+console.log(2!=='2'); // true !== - неравенство строгое
+console.log(2!='2'); // false с неравенство нестрогое
+
+
